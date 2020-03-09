8020c3bb36433189099ccec30a9245920be7eec9
Pedro Guzman <pedro.grifin@gmail.com>
2020-03-09 08:49:03 +0100

    prueba 1 finalizada y con comentarios

--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -4,10 +4,18 @@ public class ASumar {
 
 	private String valor_inicial;
 
+	/**
+	 * Constructor
+	 * @param string valor inicial.
+	 */
 	public ASumar(String string) {
 		this.valor_inicial = string;
 	}
 
+	/**
+	 * Muestra los valores junto al sumatorio
+	 * @return muestra el valor junto a la suma
+	 */
 	public String mostrar() {
 		String numero = valor_inicial;
         if (numero.length() == 1) {
