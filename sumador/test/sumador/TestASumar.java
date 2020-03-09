bb05c3050d759cd5116bb6749f8976f80aa6bbb0
Pedro Guzman <pedro.grifin@gmail.com>
2020-03-09 08:28:05 +0100

    Clase test creada

--- sumador/test/sumador/TestASumar.java
+++ sumador/test/sumador/TestASumar.java
@@ -1,5 +1,14 @@
 package sumador;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class TestASumar {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
 
 }
