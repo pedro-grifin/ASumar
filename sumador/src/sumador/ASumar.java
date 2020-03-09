ab8885dabff7c49165ed091dc77679469372774a
Pedro Guzman <pedro.grifin@gmail.com>
2020-03-09 09:13:51 +0100

    metodo mas de un digito

--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -12,16 +12,36 @@ public class ASumar {
 		this.valor_inicial = string;
 	}
 
+	
+	public int total() {
+        int suma = 0;
+
+        for (int i = 0; i < valor_inicial.length(); i++) {
+            // Cadena de un dígito.
+            String digito = valor_inicial.substring(i, 1);
+            suma = suma + Integer.getInteger(digito);
+        }
+        return suma;
+
+    }
+	
 	/**
 	 * Muestra los valores junto al sumatorio
 	 * @return muestra el valor junto a la suma
 	 */
 	public String mostrar() {
 		String numero = valor_inicial;
+		String cadena ="";
         if (numero.length() == 1) {
             return (numero + " = " + numero);
         } else if(Integer.parseInt(numero) < 0) {
         	return null;
+        } else if(numero.length()>0) {
+        	for (int i = 0; i < valor_inicial.length(); i++) {       
+            
+            cadena = valor_inicial.substring(i, i+1) + " + ";
+        	}
+        	return cadena + " = " + total();
         }
 		return null;
 	}
